0562384b40e557d3c0ad78c7372517d999c27046
sen329 <hans.hilman@gmail.com>
2016-10-17 22:38:01 +0700

    update

--- corporate sales data.cpp
+++ corporate sales data.cpp
@@ -17,10 +17,7 @@ void printCompanyDivision(CompanyDivision companydivision);
 
 int main() 
 {
-	int branch;
-	cout << "Choose branch: ";
-	cin >> branch;
-	
+	cout << "East Division"<<endl;
 	CompanyDivision east;
 	east.DivisionName = "East";
 	cout << "Type first quarter: ";
@@ -33,7 +30,54 @@ int main()
 	cin>>east.FourthQuarter;
 	east.TotalAnnualSales = east.FirstQuarter+east.SecondQuarter+east.ThirdQuarter+east.FourthQuarter;
 	east.AverageQuarterlySales = east.TotalAnnualSales/4;
-	printCompanyDivision(east);
+	
+	cout << "West Division"<<endl;
+	CompanyDivision west;
+	east.DivisionName = "West";
+	cout << "Type first quarter: ";
+	cin>> west.FirstQuarter;
+	cout << "Type second quarter: ";
+	cin>> west.SecondQuarter;
+	cout << "Type third quarter: ";
+	cin>> west.ThirdQuarter;
+	cout << "Type fourth quarter: ";
+	cin>> west.FourthQuarter;
+	west.TotalAnnualSales = west.FirstQuarter+west.SecondQuarter+west.ThirdQuarter+west.FourthQuarter;
+	west.AverageQuarterlySales = west.TotalAnnualSales/4;
+	
+	cout << "North Division"<<endl;
+	CompanyDivision north;
+	east.DivisionName = "North";
+	cout << "Type first quarter: ";
+	cin>>north.FirstQuarter;
+	cout << "Type second quarter: ";
+	cin>>north.SecondQuarter;
+	cout << "Type third quarter: ";
+	cin>>north.ThirdQuarter;
+	cout << "Type fourth quarter: ";
+	cin>>north.FourthQuarter;
+	north.TotalAnnualSales = north.FirstQuarter+north.SecondQuarter+north.ThirdQuarter+north.FourthQuarter;
+	north.AverageQuarterlySales = north.TotalAnnualSales/4;
+	
+	cout << "South Division"<<endl;
+	CompanyDivision south;
+	east.DivisionName = "South";
+	cout << "Type first quarter: ";
+	cin>>south.FirstQuarter;
+	cout << "Type second quarter: ";
+	cin>>south.SecondQuarter;
+	cout << "Type third quarter: ";
+	cin>>south.ThirdQuarter;
+	cout << "Type fourth quarter: ";
+	cin>>south.FourthQuarter;
+	south.TotalAnnualSales = south.FirstQuarter+south.SecondQuarter+south.ThirdQuarter+south.FourthQuarter;
+	south.AverageQuarterlySales = south.TotalAnnualSales/4;
+	
+	CompanyDivision divisionnamearray[]={east, west, north, south};
+	
+	for (int i = 0; i <4 ; i++){
+		printCompanyDivision(divisionnamearray[i]);
+	}
 	
 	return 0;
 }
